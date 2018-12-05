c6c3a4acbb7ac16019fd20300b246e406b207802
DESKTOP-0FHQFUK\Dario <dario.litardi@libero.it>
2018-12-05 15:50:56 +0100

    Correzione modulo traj_reconstructor

--- TrajectoriesModule/traj_reconstructor.py
+++ TrajectoriesModule/traj_reconstructor.py
@@ -21,8 +21,120 @@ def GetDistanza(position1, position2):
 
 def select_authorizer(*args):
     return sqlite3.SQLITE_OK
-
 def RecontructPathLogs():
+
+
+    pathLogUnico = "C:\\Users\\Dario\\Desktop\\HomeDesigner\\bin\\Debug\\Log\\sim_354,471915566582\\DatasetPaths.txt"
+    f = open(pathLogUnico, "r")
+    listasegmenti=list()
+    j=0
+    while(True):
+        if(j==0):
+            linea1=f.readline()
+            if(linea1==""): ##il log unico è vuoto
+                return
+            linea2=f.readline()
+            parsedline1=linea1.split(' ')
+            parsedline2=linea2.split(' ')
+
+            p0= Position()
+            p0.x=int(parsedline1[1])
+            p0.y =int(parsedline1[2])
+            p1 = Position()
+
+
+            p1.x =int(parsedline2[1])
+            p1.y =int(parsedline2[2])
+            distanza=GetDistanza(p0,p1)
+            if(distanza>35):
+                lista0=list()
+                lista1=list()
+                lista0.append(linea1)
+                lista1.append(linea2)
+                listasegmenti.append(lista0)
+                listasegmenti.append(lista1)
+
+            else:
+                lista0 = list()
+                lista0.append(linea1)
+                lista0.append(linea2)
+                listasegmenti.append(lista0)
+
+        j+=1
+        linea=f.readline()
+        if(linea=="" or j==50):
+
+
+            ConstructDatabase(listasegmenti)
+            return
+
+        parsedline=linea.split(' ')
+        p=Position()
+        p.x=int(parsedline[1])
+        p.y=int(parsedline[2])
+        timestamp=parsedline[0]
+        flag=False
+        for i in range(0,len(listasegmenti)):
+            s=listasegmenti[i][len(listasegmenti[i])-1]
+            if(s=="fine_segmento"):
+                continue
+            parsedline = s.split(' ')
+            pos = Position()
+            pos.x = int(parsedline[1])
+            pos.y = int(parsedline[2])
+            distanza=GetDistanza(p,pos)
+            timestampPos=parsedline[0]
+            if(distanza==0 and timestamp==timestampPos):
+                listasegmenti[i].append(linea)
+                listasegmenti[i].append("fine_segmento")
+                segmentonuovo = list()
+                segmentonuovo.append(linea)
+                listasegmenti.append(segmentonuovo)
+                break
+            if(distanza<=35 and timestampPos!=timestamp):
+                listasegmenti[i].append(linea)
+                break
+            if(i==len(listasegmenti)-1 and distanza>35):
+                segmentonuovo = list()
+                segmentonuovo.append(linea)
+                listasegmenti.append(segmentonuovo)
+
+
+
+
+
+
+
+
+
+
+def InserimentiInDB(segmento, c, conn):
+    id_valore_inizio=0
+    id_valore_fine=0
+    c.execute("INSERT INTO segmento VALUES (?, ?, ?);", (None, None, None))
+    id_segmento = c.execute("SELECT id_segmento FROM segmento ORDER BY id_segmento DESC LIMIT 1").fetchone()[0]
+
+    for k in range(0, len(segmento)):
+        parsedstring=segmento[k].split(' ')
+        timestamp=parsedstring[0]
+        posizione=parsedstring[1]+ " "+ parsedstring[2]
+        c.execute("INSERT INTO valore VALUES (?, ?, ?);", (None, timestamp, posizione))
+        id_valore = c.execute("SELECT id_valore FROM valore ORDER BY id_valore DESC LIMIT 1").fetchone()[0]
+
+        c.execute("INSERT INTO valore_segmento VALUES (?, ?, ?);", (id_valore, id_segmento, posizione))
+
+        if(k==0):
+
+            id_valore_inizio = c.execute("SELECT id_valore FROM valore ORDER BY id_valore DESC LIMIT 1").fetchone()[0]
+
+        if (k == len(segmento)-1):
+            id_valore_fine = c.execute("SELECT id_valore FROM valore ORDER BY id_valore DESC LIMIT 1").fetchone()[0]
+
+    c.execute("UPDATE segmento SET id_valore_inizio = ? ,id_valore_fine = ? WHERE id_segmento = ? ;", ( id_valore_inizio,id_valore_fine,id_segmento,))
+    conn.commit()
+
+
+def ConstructDatabase(listasegmenti):
     ##creo il database dove andrò a salvare i vari log
     conn = sqlite3.connect('C:\\Users\\Dario\\Desktop\\HomeDesigner\\bin\\Debug\\Log\\sim_354,471915566582\\trajectoriesDB.db')
     conn.set_authorizer(select_authorizer)
@@ -39,15 +151,16 @@ def RecontructPathLogs():
     conn.commit()
 
 
-    pathLogUnico= "C:\\Users\\Dario\\Desktop\\HomeDesigner\\bin\\Debug\\Log\\sim_354,471915566582\\DatasetPaths.txt"
-    f=open(pathLogUnico,"r")
-    j=0
-    p0=None
-    p1=None
-    timestamppos0=""
-    timestamppos1=""
-    lista=list()
 
+
+    for i in range(0, len(listasegmenti)):
+
+        InserimentiInDB(listasegmenti[i], c, conn)
+
+    conn.close()
+
+
+'''''
     while(True):
         if(j==0):
             linea1=f.readline()
@@ -116,7 +229,7 @@ def RecontructPathLogs():
 
         j+=1
         linea = f.readline()
-        if(linea=="" or j==10):
+        if(linea=="" or j==20):
             conn.close()
             return
 
@@ -219,6 +332,7 @@ def RecontructPathLogs():
 
             c.execute("INSERT INTO segmento VALUES (?, ?, ?);", (None, id_valore, id_valore))
             id_seg = c.execute("SELECT id_segmento FROM segmento ORDER BY id_segmento DESC LIMIT 1").fetchone()[0]
+
             id_valore = c.execute("SELECT id_valore FROM valore ORDER BY id_valore DESC LIMIT 1").fetchone()[0]
 
             c.execute("INSERT INTO valore_segmento VALUES (?, ?, ?);", (id_valore, id_seg, s))
@@ -229,3 +343,4 @@ def RecontructPathLogs():
             lista[0] = lista[1]
             lista[1] = id_seg
             continue
+'''''
